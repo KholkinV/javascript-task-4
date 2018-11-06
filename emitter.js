6e5566b1f7388dea9af9ebbd5a77953eda68249d
Vadik Kholkin <kholkinV@outlook.com>
2018-11-06 17:32:12 +0500

    Initial

--- emitter.js
+++ emitter.js
@@ -11,6 +11,12 @@ const isStar = true;
  * @returns {Object}
  */
 function getEmitter() {
+    let events = [];
+
+    function isChildEvent(event, childEvent) {
+        return childEvent.indexOf(event) === 0 && childEvent[event.length] === '.';
+    }
+
     return {
 
         /**
@@ -18,26 +24,58 @@ function getEmitter() {
          * @param {String} event
          * @param {Object} context
          * @param {Function} handler
+         * @returns {Object} this
          */
         on: function (event, context, handler) {
-            console.info(event, context, handler);
+            events.push({ event, context, handler });
+
+            return this;
         },
 
         /**
          * Отписаться от события
          * @param {String} event
          * @param {Object} context
+         * @returns {Object} this
          */
         off: function (event, context) {
-            console.info(event, context);
+            events = events.filter(record => {
+                return !(record.context === context &&
+                    (event === record.event || isChildEvent(event, record.event)));
+            });
+
+            return this;
         },
 
         /**
          * Уведомить о событии
          * @param {String} event
+         * @returns {Object} this
          */
         emit: function (event) {
-            console.info(event);
+            let records = events.filter(record => {
+                return (event === record.event || isChildEvent(record.event, event));
+            });
+
+            records.forEach(record => {
+                record.times--;
+                record.callCount++;
+            });
+
+            records = records.filter(record => {
+                return (!record.frequency || record.callCount % record.frequency === 1) &&
+                (!record.times || record.times > 0);
+            });
+
+            records.sort((a, b) => {
+                return a.event.split('.').length < b.event.split('.').length;
+            });
+
+            records.forEach(record => {
+                record.handler.call(record.context);
+            });
+
+            return this;
         },
 
         /**
@@ -47,9 +85,16 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} times – сколько раз получить уведомление
+         * @returns {Object} this
          */
         several: function (event, context, handler, times) {
-            console.info(event, context, handler, times);
+            if (times <= 1) {
+                this.on(event, context, handler);
+            } else {
+                events.push({ event, context, handler, times });
+            }
+
+            return this;
         },
 
         /**
@@ -59,9 +104,17 @@ function getEmitter() {
          * @param {Object} context
          * @param {Function} handler
          * @param {Number} frequency – как часто уведомлять
+         * @returns {Object} this
          */
         through: function (event, context, handler, frequency) {
-            console.info(event, context, handler, frequency);
+            let callCount = 0;
+            if (frequency <= 1) {
+                this.on(event, context, handler);
+            } else {
+                events.push({ event, context, handler, frequency, callCount });
+            }
+
+            return this;
         }
     };
 }
